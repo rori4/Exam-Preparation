09bb17e3f4f94835c5925445e478fcdb55230e60
Rangel <Rangel@DESKTOP-FDI4JOA>
2016-10-21 19:39:50 +0300

    changesd some code again

---
+++ RageQuit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
