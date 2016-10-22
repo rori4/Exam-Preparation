ef5eae655382478d6f4d63d88a81ab02e6467951
Rangel <Rangel@DESKTOP-FDI4JOA>
2016-10-22 14:43:19 +0300

    DONE SOLUTION

---
+++ Files 2 from Video/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files_2_from_Video
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<string,Dictionary<string,int>> putput = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine().Split('\\');
+            }
+        }
+    }
+}
